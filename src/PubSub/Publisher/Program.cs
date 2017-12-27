c02c6273022aaae725f04980ef76189a02018a8e
Uong Dong Hung <hungud_x86@hotmail.com>
2017-12-27 23:52:53 +0700

    UPT PUBSUB IN NETMQ

---
+++ src/PubSub/Publisher/Program.cs
@@ -0,0 +1,45 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random(50);
+
+            using (var pubSocket = new PublisherSocket())
+            {
+                Console.WriteLine("Publisher socket binding...");
+                pubSocket.Options.SendHighWatermark = 1000;
+                pubSocket.Bind("tcp://localhost:12345");
+
+                for (var i = 0; i < 100; i++)
+                {
+                    var randomizedTopic = rand.NextDouble();
+                    if (randomizedTopic > 0.5)
+                    {
+                        var msg = "TopicA msg-" + i;
+                        Console.WriteLine("Sending message : {0}", msg);
+                        pubSocket.SendMoreFrame("TopicA").SendFrame(msg);
+                    }
+                    else
+                    {
+                        var msg = "TopicB msg-" + i;
+                        Console.WriteLine("Sending message : {0}", msg);
+                        pubSocket.SendMoreFrame("TopicB").SendFrame(msg);
+                    }
+
+                    Console.WriteLine("ENTER TO SEND MORE MESSAGE");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ src/PubSub/Subcriber/Program.cs
@@ -0,0 +1,46 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subcriber
+{
+    class Program
+    {
+        public static IList<string> allowableCommandLineArgs
+           = new[] { "TopicA", "TopicB", "All" };
+
+        static void Main(string[] args)
+        {
+            //if (args.Length != 1 || !allowableCommandLineArgs.Contains(args[0]))
+            //{
+            //    Console.WriteLine("Expected one argument, either " +
+            //                      "'TopicA', 'TopicB' or 'All'");
+            //    Environment.Exit(-1);
+            //}
+
+            //string topic = args[0] == "All" ? "" : args[0];
+            string topic = "";
+            Console.WriteLine("Subscriber started for Topic : {0}", topic);
+
+            using (var subSocket = new SubscriberSocket())
+            {
+                subSocket.Options.ReceiveHighWatermark = 1000;
+                subSocket.Connect("tcp://localhost:12345");
+                subSocket.Subscribe(topic);
+                Console.WriteLine("Subscriber socket connecting...");
+                while (true)
+                {
+                    string messageTopicReceived = subSocket.ReceiveFrameString();
+                    string messageReceived = subSocket.ReceiveFrameString();
+                    Console.WriteLine(messageReceived);
+                    Console.WriteLine("Task delay");
+                    Task.Delay(5000).Wait();
+                }
+            }
+        }
+    }
+}
